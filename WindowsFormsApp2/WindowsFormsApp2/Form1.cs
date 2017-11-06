5c26001ccf00539c29d1ff2f9b627ba63c8f1466
Manuel <manuepeva@gmail.com>
2017-11-06 17:19:48 -0500

    angular web api with sql server and identity authentication

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+       protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS = "data source=.SQLEXPRESS; database= EMPLOYEESTABLE; integrated security=SSPI";
+            SqlConnection con = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("Select * from EMPLOYEESTABLE", con);
+            con.Open();
+            DataGridView.DataGridViewControlCollection = cmd.ExecuteReader();
+            DataGridView.DataGridViewControlCollection();
+            con.Close();
+        }
+    }
+}
